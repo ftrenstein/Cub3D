b85c9f93343f6168f0c9ad08ff198128de9d4b93
Your Name <you@example.com>
2023-04-03 18:35:46 +0200

    Fixed norm in map validation

--- bonuses/srcs/map/val_map2_bonus.c
+++ bonuses/srcs/map/val_map2_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mlakenya <mlakenya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 15:44:37 by renstein          #+#    #+#             */
-/*   Updated: 2023/04/03 18:10:58 by mlakenya         ###   ########.fr       */
+/*   Updated: 2023/04/03 18:35:13 by mlakenya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,33 @@
 
 int	ft_search(t_params *all, char **used, int i, int j)
 {
-	if(i > all->map_height || i < 0 ||  j >  all->map_width || j < 0)
-		return(1);
-	if(all->map[i][j] == '1')
-		return(0);
+	if (i > all->map_height || i < 0 || j > all->map_width || j < 0)
+		return (1);
+	if (all->map[i][j] == '1')
+		return (0);
 	if (used[i][j] == '1')
-		return(0);
+		return (0);
 	used[i][j] = '1';
-	if (ft_search(all, used, i, j+1) == 1)
-		return(1);
-	if (ft_search(all, used, i, j-1) == 1)
-		return(1);	
-	if (ft_search(all, used, i+1, j) == 1)
-		return(1);
-	if (ft_search(all, used, i-1, j) == 1)
-		return(1);
+	if (ft_search(all, used, i, j + 1) == 1)
+		return (1);
+	if (ft_search(all, used, i, j - 1) == 1)
+		return (1);
+	if (ft_search(all, used, i + 1, j) == 1)
+		return (1);
+	if (ft_search(all, used, i - 1, j) == 1)
+		return (1);
 	return (0);
 }
 
-void ft_check_wall(t_params *all, int i, int j)
+void	ft_check_wall(t_params *all, int i, int j)
 {
-	char **used;
-	int k;
+	char	**used;
+	int		k;
+
 	k = 0;
 	used = malloc(all->map_height * sizeof(char *));
-	while( k < all->map_height)
-		used[k++] = (char *) malloc((sizeof(char *) * all->map_width));
+	while (k < all->map_height)
+		used[k++] = (char *)malloc((sizeof(char *) * all->map_width));
 	ft_memset(used, 0, sizeof(used));
 	if (ft_search(all, used, i, j) == 1)
 		ft_error(11, NULL);
